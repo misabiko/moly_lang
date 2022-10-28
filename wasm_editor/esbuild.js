d94c74464a1777659e691218d137e6d784c2d625
misabiko <misabiko@gmail.com>
2022-10-28 14:50:13 -0400

    Adding logs to debug ci

--- wasm_editor/esbuild.js
+++ wasm_editor/esbuild.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+console.log('Building entry.ts');
 require("esbuild").build({
 	entryPoints: ['entry.ts'],
 	bundle: true,
@@ -7,6 +8,7 @@ require("esbuild").build({
 	sourcemap: true,
 })
 	.then(() => {
+		console.log('Copying html and wasm');
 		fs.copyFileSync('./index.html', './dist/index.html')
 		fs.copyFileSync('./pkg/moly_bg.wasm', './dist/moly_bg.wasm')
 	})
